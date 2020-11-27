e8d6fec2126d859395b99cdf43e7b9c5c7b5c227
Matilde24 <matyarostegui@gmail.com>
2020-11-27 11:20:43 -0600

    Variables

---
+++ Tipos.kt
@@ -0,0 +1,9 @@
+
+fun main()
+{
+    val int = 123 val long = 123456L 
+    val double = 12.34 
+    val float = 12.34F 
+    val hexadecimal = 0xAB 
+    val binary = 0b01010101
+}
---
+++ Variables.kt
@@ -0,0 +1,16 @@
+
+fun main()
+{
+    var nombre = "Excy"
+    nombre = "Matilda"
+
+    println("Mi nombre es: $nombre")
+
+    val apellido = "Lopez"
+    apellido = "Arostegui"
+
+      println ("Mi apellido es: $apellido")
+     
+     var edad = "60"
+     println("mi nombre es $nombre $apellido y tengo $edad años")
+}
