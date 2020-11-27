9608398a608f2712fc7c95d585709f4cc2c5fccf
Matilde24 <matyarostegui@gmail.com>
2020-11-27 13:02:33 -0600

    Tipos.kt

--- Tipos.kt
+++ Tipos.kt
@@ -1,9 +1,30 @@
 
 fun main()
 {
-    val int = 123 val long = 123456L 
-    val double = 12.34 
-    val float = 12.34F 
-    val hexadecimal = 0xAB 
-    val binary = 0b01010101
+    //numeros
+val int = 123
+val long = 123456L
+val double = 12.34
+val float = 12.34F
+val hexadecimal = 0xAB
+val binary = 0b01010101
+
+println("int $int, long $long, double $double, float $float, hexadecimal $hexadecimal, binary $binary")
+
+//cadenas
+val string = "string con \n una línea nueva"
+val rawString = """
+ raw string es útil para
+ cadenas con muchas líneas
+ """
+println(" $string $rawString")
+
+    //booleanos 
+    val x = 1 
+    val y = 2 
+    val z = 2 
+    val esTrue = x < y && x < z 
+    val otroTrue = x == y || y == z
+
+    
 }
